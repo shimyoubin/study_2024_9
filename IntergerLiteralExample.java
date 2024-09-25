b12e6df7716659ef3bf873b52ff809d940a109ad
shimyoubin <122425887+shimyoubin@users.noreply.github.com>
2024-09-25 09:40:11 +0900

    Add files via upload

---
+++ IntergerLiteralExample.java
@@ -0,0 +1,17 @@
+package ch02.sce01;
+
+public class IntergerLiteralExample {
+
+	public static void main(String[] args) {
+		int v1 = 0b1011;
+		int v2 = 0206;
+		int v3 = 365;
+		int v4 = 0xB3;
+		
+		System.out.println("var1: " + v1);
+		System.out.println("var2: " + v2);
+		System.out.println("var3: " + v3);
+		System.out.println("var4: " + v4);
+	}
+
+}
