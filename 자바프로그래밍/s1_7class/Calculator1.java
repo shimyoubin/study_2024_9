868b870cf7083c5c43e0798ac710d85b2e21856f
shimyoubin <122425887+shimyoubin@users.noreply.github.com>
2024-10-26 11:11:47 +0900

    Add files via upload

---
+++ 자바프로그래밍/s1_7class/Calculator1.java
@@ -0,0 +1,12 @@
+package ch011_2_1;
+
+public class Calculator1 {
+	double areaRactangle(double width) {
+		return width * width;
+	}
+	
+	double areaRactagle(double width, double hight) {
+		return width * hight;
+	}
+
+}
