db9db676af0ae956e78e2b8057ca9b85c610bee0
shimyoubin <122425887+shimyoubin@users.noreply.github.com>
2024-11-11 12:47:00 +0900

    Add files via upload

---
+++ 자바프로그래밍/s1_9class/Animal.java
@@ -0,0 +1,9 @@
+package ch16.D1108;
+
+public abstract class Animal {
+	public void breathe()	{
+		System.out.println("숨을 쉽니다.");
+	}
+	
+	public abstract void sound();
+}
---
+++ 자바프로그래밍/s1_9class/Cat.java
@@ -0,0 +1,7 @@
+package ch16.D1108;
+
+public class Cat extends Animal{
+	public void sound() {
+		System.out.println("냐옹");
+	}
+}
